4ad616d9170eda2a327b8890979fe7b546113350
Lee D Jones <leedj@SURFACE>
2021-05-07 10:23:59 +0100

    Pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello AWS! Lets see if this pushes automatically'));
+app.get('/', (req, res) => res.send('Hello AWS! Lets see if this pushes automatically for a second time'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
